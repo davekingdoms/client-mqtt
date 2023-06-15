58210f4119690f776a106079afe583c144c0cb10
davekingdoms <regni.alessandro@gmail.com>
2023-06-15 11:04:03 +0200

    Formatted using cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use csv::WriterBuilder;
-use drogue_ttn::v3::{Message, Payload, DataRate};
+use drogue_ttn::v3::{DataRate, Message, Payload};
 use futures::{executor::block_on, stream::StreamExt};
 use paho_mqtt::{self as mqtt};
-use std::{process, time::Duration, fs::{OpenOptions}};
 use std::path::Path;
+use std::{fs::OpenOptions, process, time::Duration};
 
 // TODO: Replace with your own TTN identifiers
 const HOST: &str = "eu1.cloud.thethings.network:1883";
@@ -19,8 +19,6 @@ fn main() {
     // Initialize the logger from the environment
     env_logger::init();
 
-
-
     // Create the client. Use a Client ID for a persistent session.
     // A real system should try harder to use a unique ID.
 
@@ -35,16 +33,16 @@ fn main() {
         let mut strm = cli.get_stream(244);
 
         // Define the set of options for the connection
-      let lwt = mqtt::Message::new("test", "Async subscriber lost connection", mqtt::QOS_1);
+        let lwt = mqtt::Message::new("test", "Async subscriber lost connection", mqtt::QOS_1);
 
         // Create the connect options, explicitly requesting MQTT v3.x
         let opts = mqtt::ConnectOptionsBuilder::new()
-        .user_name(USERNAME)
-        .password(PASSWORD)
-        .keep_alive_interval(Duration::from_secs(30))
-        .clean_session(false)
-        .will_message(lwt)
-        .finalize();
+            .user_name(USERNAME)
+            .password(PASSWORD)
+            .keep_alive_interval(Duration::from_secs(30))
+            .clean_session(false)
+            .will_message(lwt)
+            .finalize();
 
         // Make the connection to the broker
         println!("Connecting to the MQTT server...");
@@ -53,36 +51,40 @@ fn main() {
         println!("Subscribing to topics: {:?}", TOPICS);
         cli.subscribe_many(&TOPICS, &QOS).await?;
 
-       let path = "/home/alleregni/csvexample.csv";
-  
-      let mut options = OpenOptions::new();
-      options.write(true).create(true).append(true);
-  
-      let file_exists = Path::new(path).exists();
-  
-      let mut csv_writer = match options.open(path) {
-          Ok(file) => {
-              if file_exists {
-                  WriterBuilder::new()
-                      .has_headers(false)
-                      .from_writer(file)
-              } else {
-                  let mut csv_writer = WriterBuilder::new()
-                      .has_headers(true)
-                      .from_writer(file);
-  
-                  let header = ["GatewayId", "UTC Time","Latitude","Longitude","Altitude","RSSI","SNR","Spreading Factor"];
-                  csv_writer.write_record(&header).expect("Errore durante la scrittura dell'intestazione");
-                  csv_writer.flush().expect("Errore durante il flushing del writer");
-  
-                  csv_writer
-              }
-              
+        let path = "/home/alleregni/csvexample.csv";
 
-              
-          }
-          Err(_) => panic!("Impossibile aprire il file"),
-      };
+        let mut options = OpenOptions::new();
+        options.write(true).create(true).append(true);
+
+        let file_exists = Path::new(path).exists();
+
+        let mut csv_writer = match options.open(path) {
+            Ok(file) => {
+                if file_exists {
+                    WriterBuilder::new().has_headers(false).from_writer(file)
+                } else {
+                    let mut csv_writer = WriterBuilder::new().has_headers(true).from_writer(file);
+
+                    let header = [
+                        "GatewayId",
+                        "UTC Time",
+                        "Latitude",
+                        "Longitude",
+                        "Altitude",
+                        "RSSI",
+                        "SNR",
+                        "Spreading Factor",
+                    ];
+                    csv_writer
+                        .write_record(&header)
+                        .expect("Header writing error");
+                    csv_writer.flush().expect("Error during writer flushing ");
+
+                    csv_writer
+                }
+            }
+            Err(_) => panic!("Unable to open file"),
+        };
         // Just loop on incoming messages.
         println!("Waiting for messages...");
 
@@ -93,54 +95,66 @@ fn main() {
         while let Some(msg_opt) = strm.next().await {
             if let Some(raw) = msg_opt {
                 if let Ok(payload) =
-                serde_json::from_slice::<Message>(raw.payload()).map(|message| message.payload)
-            {
-                match payload {
-                    Payload::JoinAccept(_) => println!("device joined"),
+                    serde_json::from_slice::<Message>(raw.payload()).map(|message| message.payload)
+                {
+                    match payload {
+                        Payload::JoinAccept(_) => println!("device joined"),
 
+                        Payload::Uplink(uplink) => {
+                            let buf = uplink.frame_payload;
+                            println!("frame payload: {:?}", buf);
 
-                    Payload::Uplink(uplink) => {
+                            println!("{:?}", buf.len());
 
-                        let buf = uplink.frame_payload;
-                        println!("frame payload: {:?}",buf);
+                            let latitude =
+                                Some(f64::from_le_bytes(buf[0..8].try_into().unwrap())).unwrap();
+                            println!("latitude {:?}", latitude);
+                            let longitude =
+                                Some(f64::from_le_bytes(buf[8..16].try_into().unwrap())).unwrap();
+                            println!("Longitude {:?}", longitude);
+                            let altitude =
+                                Some(f32::from_le_bytes(buf[16..20].try_into().unwrap())).unwrap();
+                            println!("altitude {:?}", altitude);
 
-                        println!("{:?}", buf.len());
+                            let time = uplink.received_at;
+                            let sf = match uplink.settings.data_rate {
+                                Some(DataRate::Lora(data_rate_lora)) => {
+                                    data_rate_lora.spreading_factor
+                                }
+                                _ => 0, // Valore di fallback nel caso in cui il campo sia None o non sia di tipo Lora
+                            };
+                            println!("Sf: {}", sf);
 
-                        let latitude = Some(f64::from_le_bytes(buf[0..8].try_into().unwrap())).unwrap();
-                        println!("latitude {:?}", latitude);
-                        let longitude = Some(f64::from_le_bytes(buf[8..16].try_into().unwrap())).unwrap();
-                        println!("Longitude {:?}", longitude);
-                        let altitude = Some(f32::from_le_bytes(buf[16..20].try_into().unwrap())).unwrap();              
-                        println!("altitude {:?}", altitude);
-                       
-                       let time = uplink.received_at;
-                       let sf = match uplink.settings.data_rate {
-                        Some(DataRate::Lora(data_rate_lora)) => data_rate_lora.spreading_factor,
-                        _ => 0, // Valore di fallback nel caso in cui il campo sia None o non sia di tipo Lora
-                    };
-                    println!("Sf: {}",sf);
-
-                        let mut i = 0;
-                        while i < uplink.rx_metadata.len(){
-                            let gateway_id =  uplink.rx_metadata[i].gateway_ids.get("gateway_id").unwrap();
-                            if gateway_id == "dlos"{
-                                let snr = uplink.rx_metadata[i].snr.unwrap();
-                                let rssi = uplink.rx_metadata[i].rssi;
-                                println!("SNR: {}, RSSI:{}",snr,rssi);
-                                csv_writer.write_record(&[gateway_id.to_string(), time.to_string(), latitude.to_string(), longitude.to_string(), altitude.to_string(), rssi.to_string(), snr.to_string(),sf.to_string()]).expect("Errore durante la scrittura del record");
-                                csv_writer.flush().expect("Errore durante il flushing del writer");
-                                break;
-                                 }
-
-                            else{i +=1;}
+                            let mut i = 0;
+                            while i < uplink.rx_metadata.len() {
+                                let gateway_id =
+                                    uplink.rx_metadata[i].gateway_ids.get("gateway_id").unwrap();
+                                if gateway_id == "dlos" {
+                                    let snr = uplink.rx_metadata[i].snr.unwrap();
+                                    let rssi = uplink.rx_metadata[i].rssi;
+                                    println!("SNR: {}, RSSI:{}", snr, rssi);
+                                    csv_writer
+                                        .write_record(&[
+                                            gateway_id.to_string(),
+                                            time.to_string(),
+                                            latitude.to_string(),
+                                            longitude.to_string(),
+                                            altitude.to_string(),
+                                            rssi.to_string(),
+                                            snr.to_string(),
+                                            sf.to_string(),
+                                        ])
+                                        .expect("Errore durante la scrittura del record");
+                                    csv_writer.flush().expect("Error during writer flushing");
+                                    break;
+                                } else {
+                                    i += 1;
+                                }
                             }
-
                         }
-                        
                     }
-                 }
-            }
-            else {
+                }
+            } else {
                 // A "None" means we were disconnected. Try to reconnect...
                 println!("Lost connection. Attempting reconnect.");
                 while let Err(err) = cli.reconnect().await {
@@ -150,13 +164,10 @@ fn main() {
                 }
             }
         }
-        
-     
+
         // Explicit return type for the async block
         Ok::<(), mqtt::Error>(())
     }) {
         eprintln!("{}", err);
     }
-
-   
 }
